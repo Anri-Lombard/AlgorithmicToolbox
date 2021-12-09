35e0fddeac9386dc9be115f77a9a40ddf1b45723
Anri Lombard <anri.m.lombard@gmail.com>
2021-12-09 16:39:50 +0200

    DP is difficult, for now

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -20,10 +20,35 @@ vector<int> optimal_sequence(int n) {
   return sequence;
 }
 
+vector<int> dyn_seq(int n) {
+ std::vector<int> sequence;
+	std::vector<int>  arr(n+1);
+	for(int i =1; i< arr.size(); i++){
+		arr[i] = arr[i-1] + 1;
+		if (i%2 == 0) 
+			arr[i] = std::min(1 + arr[i/2], arr[i]);
+		if (i%3 == 0) 
+			arr[i] = std::min(1 + arr[i/3], arr[i]);
+	}
+	int i = n;
+	while (i>1) {
+		sequence.push_back(i);
+		if (arr[i-1] == arr[i]-1)
+			i = i-1;
+		else if (i%2 == 0 && (arr[i/2] == arr[i]-1))
+			i = i/2;
+		else if (i%3 == 0 && (arr[i/3] == arr[i]-1))
+			i = i/3;
+	}
+	sequence.push_back(1);
+	reverse(sequence.begin(), sequence.end());
+	return sequence;
+}
+
 int main() {
   int n;
   std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
+  vector<int> sequence = dyn_seq(n);
   std::cout << sequence.size() - 1 << std::endl;
   for (size_t i = 0; i < sequence.size(); ++i) {
     std::cout << sequence[i] << " ";
--- week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 int edit_distance(const string &str1, const string &str2) {
   //write your code here
-  return 0;
+  int a = str1.size();
+	int b = str2.size();
+	vector<vector<int> > disp(a + 1, vector<int>(b + 1, 0));
+	for(int y = 0; y <= a; ++y) {
+        disp[y][0] = y;
+    }
+    for(int z = 0; z <= b; ++z) {
+        disp[0][z] = z;
+    }
+	int cal_cost = 0;
+	for(int i = 1; i <= a; ++i) {
+		for(int j = 1; j <= b; ++j) {
+			if(str1[i-1] == str2[j-1])
+				cal_cost = 0;
+			else
+				cal_cost = 1;
+			disp[i][j] = std::min(std::min(disp[i-1][j],disp[i][j-1]) + 1, disp[i-1][j-1] + cal_cost);
+		}
+	}
+	return disp[a][b];
 }
 
 int main() {
